a498d91ec95554800f35bb59ccfcd2bd7bacde02
Edisonfb <edison.fbnts@gmail.com>
2019-12-13 07:30:09 -0300

    tiro

--- Assets/Shoot.cs
+++ Assets/Shoot.cs
@@ -4,15 +4,47 @@ using UnityEngine;
 
 public class Shoot : MonoBehaviour
 {
+    public float force;
+    public GameObject bulletPrefab;
+    public Transform initialPosition;
+    public bool canShoot;
+    public Vector3 direction;
+    public Camera camera;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        canShoot = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetMouseButtonDown(0))
+        {
+
+            canShoot = true;
+        }
+        else
+        {
+            canShoot = false;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if(canShoot)
+        {
+            direction = camera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0));
+            direction = direction - gameObject.transform.position;
+            direction.Normalize();
+            Shooting(direction);
+        }
+    }
+
+    public void Shooting(Vector3 distance)
+    {
+        GameObject aux = Instantiate(bulletPrefab, initialPosition.position, initialPosition.rotation);
+        aux.GetComponent<Rigidbody>().AddForce(distance * force);
     }
 }
